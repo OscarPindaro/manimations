f7b69915d305df909bbfa5d8a6415a339c5955a1
Oscar Pindaro <oscar.pindaro@gmail.com>
2024-09-14 19:25:26 +0200

    linestar object now is a bit better

--- scenes/linestar/linestar.py
+++ scenes/linestar/linestar.py
@@ -1,97 +1,101 @@
 from manim import *
+from dataclasses import dataclass, field
 import numpy as np
 from typing import List, Tuple
-from dataclasses import dataclass
 
-class VeryBasicLineStar(Scene):
-    
-    N_SAMPLES: int = 100
-    Y_RANGE: Tuple[int,int] = (-3.5,3.5)
-    X_RANGE: Tuple[int,int] = (-3.5,3.5)
-    
-    def construct(self):
-        
-        
-        assert self.N_SAMPLES % 2 == 0, "N_SAMPLE should be even"
-        
-        x_axis, y_axis = get_x_y_coords(self.X_RANGE, self.Y_RANGE, self.N_SAMPLES)
-        assert y_axis.shape == x_axis.shape
-        lines: List[Line]  = []
-        for x, y in zip(x_axis,y_axis):
-            lines.append(Line((0,y,0), (x,0,0)))
-
-        self.play(Create(line) for line in lines)
-        
-        
-class LaggedLineStar(Scene):
-    
-    N_SAMPLES: int = 20
-    Y_RANGE: Tuple[int,int] = (-3.5,3.5)
-    X_RANGE: Tuple[int,int] = (-3.5,3.5)
-    
-    def construct(self):
-        
-        
-        assert self.N_SAMPLES % 2 == 0, "N_SAMPLE should be even"
-        
-        x_axis, y_axis = get_x_y_coords(self.X_RANGE, self.Y_RANGE, self.N_SAMPLES)
-        assert y_axis.shape == x_axis.shape
-        lines: List[Line]  = []
-        for x, y in zip(x_axis,y_axis):
-            lines.append(Line((0,y,0), (x,0,0)))
-
-        self.play(LaggedStart([Create(line) for line in lines]))
-        
-        
-def get_x_y_coords(x_range: Tuple[int,int], y_range: Tuple[int,int], n_samples: int) -> Tuple[np.ndarray, np.ndarray]: 
-        y_axis = np.concatenate(
-            (
-            np.linspace(y_range[1],y_range[0], n_samples, endpoint=True), 
-            np.linspace(y_range[0],y_range[1], n_samples, endpoint=True)
-            )
-        )
-        x_axis = np.concatenate(
-            (
-                np.linspace(0,x_range[0],n_samples//2, endpoint=True),
-                np.linspace(x_range[0],0,n_samples//2, endpoint=True),
-                np.linspace(0,x_range[1],n_samples//2, endpoint=True),
-                np.linspace(x_range[1],0,n_samples//2, endpoint=True),
-            )
-        )
-        return x_axis, y_axis
-        
-
-class ColoredLineStar(Scene):
-    
-    N_SAMPLES: int = 100
-    Y_RANGE: Tuple[int,int] = (-3.5,3.5)
-    X_RANGE: Tuple[int,int] = (-3.5,3.5)
-    PALETTE_0 = color_gradient([PURPLE, WHITE, YELLOW], 2*N_SAMPLES)
-    
-    def construct(self):
-        
-        
-        assert self.N_SAMPLES % 2 == 0, "N_SAMPLE should be even"
-        
-        x_axis, y_axis = get_x_y_coords(self.X_RANGE, self.Y_RANGE, self.N_SAMPLES)
-        assert y_axis.shape == x_axis.shape
-        lines: List[Line]  = []
-        
-        for x, y, color in zip(x_axis,y_axis, self.PALETTE_0):
-            lines.append(Line((0,y,0), (x,0,0), color=color))
-
-        self.play(Create(line) for line in lines)
-        
-Square()
-Circle()
+from src.colors import ONEDARK_CLASS_RAINBOW
 
 @dataclass
 class LineStar:
-    x_range: Tuple[int,int]   
-    y_range: Tuple[int,int] 
-    axis_resolution: int
+    radius: float
+    n: int
+    colors: List[ManimColor] = field(default_factory=list)
+    grouped_lines: VGroup = field(default_factory=lambda: VGroup(), repr=False, init=False)  # Hidden from repr
+    lines: List[Line] = field(default_factory=list, repr=False, init=False) 
+    gradient: List[ManimColor] = field(default_factory=list, repr=False, init=False)
 
-
-config.pixel_width = config.pixel_height
-config.frame_width = config.frame_height
+    def __post_init__(self):
+        # Private numpy array that is linearly spaced from 0 to radius with n steps
+        self._linear_space: np.ndarray = np.linspace(0, self.radius, self.n)
         
+        if len(self.colors) > 0:
+            # the number of colors is n*4 because in the for loop i create 4 lines
+            self.gradient = color_gradient(self.colors, self.n*4 )
+            
+        
+        # let's create the lines. I'll create them from the inner to the outer with a pattern that is hard to explai
+        for i in range(len(self._linear_space)):
+            inner = self._linear_space[i]
+            outer = self._linear_space[len(self._linear_space) - i -1]
+            self.lines.append(
+                self._create_line(
+                    (0,outer,0),
+                    (inner,0,0),
+                    color=None if len(self.gradient) == 0 else self.gradient[4*i+0] 
+                    )
+                )
+            self.lines.append(
+                self._create_line(
+                    (inner,0,0),
+                    (0,-outer,0),
+                    color=None if len(self.gradient) == 0 else self.gradient[4*i+1] 
+                    )
+                )
+            self.lines.append(
+                self._create_line(
+                    (0,-outer,0),
+                    (-inner,0,0),
+                    color=None if len(self.gradient) == 0 else self.gradient[4*i+2] 
+                    )
+                )
+            self.lines.append(
+                self._create_line(
+                    (-inner,0,0),
+                    (0,outer,0),
+                    color=None if len(self.gradient) == 0 else self.gradient[4*i+3] 
+                    )
+                )
+        # add it to the vgroup if you need to move it
+        self.grouped_lines.add(*self.lines)
+            
+
+    def _create_line(self, start: Tuple[float,float,float], end: Tuple[float,float,float], color: ManimColor | None = None) -> Line:
+        if color is None:
+            return Line(start, end)
+        else:
+            return Line(start, end, color=color)
+    
+    def create_lines(self) -> List[Create]:
+        return [Create(line) for line in self.lines]
+    
+    def uncreate_lines(self):
+        return self.animate_lines(Uncreate)
+    
+    def animate_lines(self, animation: Animation) -> List[Animation]:
+        return [animation(line) for line in self.lines]
+        
+    def get_linear_space(self):
+        # Public method to access the private array if needed
+        return self._linear_space
+    
+@dataclass
+class LinestarScene(Scene):
+    curr_palette: List[str | ManimColor] = field(default_factory=list, repr=False)
+    reverse_palette: bool = field(default=False)
+    background_color: ManimColor | str = field(default_factory=WHITE)
+    create_lag_ratio: float = 0.01
+    
+    def setup(self):
+        if not self.curr_palette:
+            self.curr_palette = ONEDARK_CLASS_RAINBOW
+        if self.reverse_palette:
+            self.curr_palette.reverse()
+        self.linestar = LineStar(6, 25, colors=self.curr_palette)
+    
+    def construct(self):
+        self.camera.background_color = WHITE
+        self.wait(0.25)
+        self.play(LaggedStart(*self.linestar.create_lines(), lag_ratio=self.create_lag_ratio))
+        # self.play(FadeIn(Line((0,0,0), (3,3,0))))
+        self.wait(0.25)
+        self.play(LaggedStart(*self.linestar.uncreate_lines(), lag_ratio=self.create_lag_ratio))
